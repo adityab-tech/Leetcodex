31de2e6866f1faf7f9bf8e51a0c5454baa5fa891
Aditya B <adityabaroniya@gmail.com>
2026-09-06 10:52:52 +0530

    Time: 6 ms (55.38%) | Memory: 9.6 MB (45.93%) - LeetSync

---
+++ 4411-count-rotations-with-exactly-k-equal-adjacent-pairs/count-rotations-with-exactly-k-equal-adjacent-pairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int n=s.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int score=0;
+            for(int j=0;j<n-1;j++){
+                if (s[(i+j)%n] == s[(i+j+1)%n]){
+                    score++;
+                }
+            }
+            if(score==k){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
